6ed1deea9b1e31b8870ca345dfdae713ab59c9c1
simov <simeonvelichkov@gmail.com>
2017-09-27 10:30:39 +0300

    Refresh access token only once

--- bin/index.js
+++ bin/index.js
@@ -51,11 +51,11 @@ config.purest = require('../config/purest')
 
 var fpath = path.resolve(process.cwd(), argv.auth)
 var auth = require(fpath)
-var refresh = Refresh(auth, env, fpath)
+var refresh = Refresh(config, auth, env, fpath)
 
 var notify = {
-  calendar: Calendar(config, auth[env].google, refresh),
-  googlegroups: GoogleGroups(config, auth[env].google, refresh),
+  calendar: Calendar(config, auth[env].google),
+  googlegroups: GoogleGroups(config, auth[env].google),
   twitter: Twitter(config, auth[env].twitter),
 }
 
@@ -65,16 +65,18 @@ var upcoming = events.filter((event) => ids.indexOf(event.id) === -1)
 
 
 if (upcoming.length) {
-  Promise.all(
-    argv.notify.split(',')
-      .map((provider) => notify[provider].send(upcoming))
-  )
-  .then(() => {
-    fs.writeFileSync(
-      path.resolve(process.cwd(), argv.ids),
-      JSON.stringify(ids.concat(upcoming.map((event) => event.id)), null, 2),
-      'utf8'
+  refresh('google').then(() =>
+    Promise.all(
+      argv.notify.split(',')
+        .map((provider) => notify[provider].send(upcoming))
     )
-  })
+    .then(() => {
+      fs.writeFileSync(
+        path.resolve(process.cwd(), argv.ids),
+        JSON.stringify(ids.concat(upcoming.map((event) => event.id)), null, 2),
+        'utf8'
+      )
+    })
+  )
   .catch((err) => console.error(err))
 }
